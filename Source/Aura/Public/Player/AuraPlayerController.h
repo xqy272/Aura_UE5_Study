1610fa7814103a9d28e0f5118a49a5b6bb27ab8d
QingYang <xqy272@Gmail.com>
2024-03-17 22:59:24 +0800

    1-12 高亮敌人

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -10,7 +10,7 @@
 
 class UInputMappingContext;
 class UInputAction;
-
+class IEnemyInterface;
 struct FInputActionValue;
 
 /**
@@ -23,6 +23,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
@@ -36,4 +37,8 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
 };
